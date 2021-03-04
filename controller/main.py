f9b9fcaac186fe6cbc985d6068bc604aa1d7939f
biglizards <22501070+biglizards@users.noreply.github.com>
2021-03-04 12:09:09 +0000

    fix bugs

--- controller/main.py
+++ controller/main.py
@@ -3,6 +3,7 @@ from typing import Optional
 import olympe
 from sanic import Sanic
 from sanic import exceptions
+from sanic.response import text
 
 import generate_path
 from model_generation.model_gen import GPSListener
@@ -14,31 +15,33 @@ class GodObject:
 
         self.model_gen = GPSListener(self.drone)
         self.model_gen.subscribe()
-        model = self.model_gen.models[0]  # why are there multiple?
 
-        self.generator = generate_path.PathGenerator(model=model, ground_route=path)
+        self.generator = generate_path.PathGenerator(model=self.model_gen.model, ground_route=path)
 
     def handle_gps_data(self, data):
         # pass this to the model generation, when they have a stable api
         pass
 
 
-app = Sanic()
+app = Sanic(name='Drone Controller')
 controller: Optional[GodObject] = None
 
 
-@app.route("/api/start")
+@app.post("/api/start")
 async def start(request):
     # connect to drone
     # init other modules and start their main loops
     # and save them in global scope i guess
+    print(request)
+    print(request.json)
     global controller
     if controller is not None:
         raise exceptions.InvalidUsage("Drone is already in flight!")
-    controller = GodObject(drone_ip=..., path=request.json.path)
+    controller = GodObject(drone_ip='10.202.0.1', path=request.json['path'])
+    return text('OK')
 
 
-@app.route("/api/gps")
+@app.post("/api/gps")
 async def gps(request):
     if controller is None:
         # just discard gps data until the drone starts flying
